b996f27df650d8ad199e71051925f464aba7fe54
Alnur <zholdas.alnur@gmail.com>
2024-05-03 19:52:53 +0500

    fix?

--- app/routers/history_router.py
+++ app/routers/history_router.py
@@ -14,6 +14,7 @@ from app.schemas.history_schemas import History as ScHistory
 
 router = APIRouter(tags=['history'])
 
+
 @router.get("/history/", response_model=List[ScHistory])
 async def read_history(
         skip: int = 0, limit: int = 10, history_type: str = None, db: Session = Depends(get_db),
@@ -38,7 +39,7 @@ async def read_history(
             "buyer": entry_dict["buyer"],
             "extra_info": entry_dict["extra_info"],
             "before_change": entry_dict["before_change"],
-            "after_change": json.dumps(after_change_json, ensure_ascii=False),
+            "after_change": json.dumps(after_change_json, ensure_ascii=True),
             "history_type": entry_dict["history_type"],
             "title": entry_dict["title"],
             "id": entry_dict["id"],
@@ -52,23 +53,19 @@ async def read_history(
     return corrected_history_entries
 
 
-
 @router.get("/history/search/", response_model=List[ScHistory])
 async def search_history(
         query_string: str, db: Session = Depends(get_db),
         current_user: User = Depends(get_current_user)
 ):
-    # Разбиваем строку запроса на отдельные слова
     search_terms = query_string.split()
 
-    # Формируем условие для поиска по текстовым колонкам
     search_filter = or_(
         *[getattr(History, column_name).ilike(f'%{term}%') for term in search_terms
           for column_name in ['username', 'buyer', 'extra_info', 'before_change',
                               'after_change', 'history_type', 'title']]
     )
 
-    # Выполняем запрос с учетом фильтрации
     history_entries = (
         db.query(History)
         .filter(search_filter)
@@ -76,7 +73,6 @@ async def search_history(
         .all()
     )
 
-    # Преобразуем результаты запроса в соответствующий формат
     corrected_history_entries = []
     for entry in history_entries:
         after_change_json = json.loads(entry.after_change.replace('\\"', ''))
